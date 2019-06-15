e6b1d30b19b29d8936a80add4c6979c1384ab422
Alejandro Bolano <elbola.cuba1992@gmail.com>
2019-06-15 23:52:56 +0200

    [Issue4]

---
+++ FileManager.DataAccess.DaoTests/Implementations/ManageFile/XmlFileTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileManager.DataAccess.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.DataAccess.Dao.Tests
+{
+    [TestClass()]
+    public class XmlFileTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
