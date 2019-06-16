03b99b1a6252d5e379fd50ac7700a4beb1c23131
Alejandro Bolano <elbola.cuba1992@gmail.com>
2019-06-16 20:00:45 +0200

    [Issue4] Update all test of StudentDao - close #4

---
+++ FileManager.DataAccess.DaoTests/Implementations/Dao/StudentDaoTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileManager.DataAccess.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManager.Common.Models;
+using FileManager.Common.Models.Util;
+
+namespace FileManager.DataAccess.Dao.Tests
+{
+    [TestClass()]
+    public class StudentDaoTests
+    {
+        Student student;
+        IStudentDao studentDao;
+        public StudentDaoTests()
+        {
+            student = new Student()
+            {
+                StudentId = 99,
+                Name = "Lolo",
+                Surname = "Manolo",
+                DateOfBirth = Convert.ToDateTime("10/02/2010")
+            };
+            studentDao = new StudentDao();
+        }
+        [TestMethod()]
+        [DataRow(EnumType.TXT)]
+        public void AddTxtTest(EnumType type)
+        { 
+            Assert.IsTrue(studentDao.Add(student, type).StudentId == student.StudentId);
+        }
+
+        [TestMethod()]
+        [DataRow(EnumType.XML)]
+        public void AddXmlTest(EnumType type)
+        {
+            Assert.IsTrue(studentDao.Add(student, type).StudentId == student.StudentId);
+        }
+
+        [TestMethod()]
+        [DataRow(EnumType.JSON)]
+        public void AddJsonTest(EnumType type)
+        {
+            Assert.IsTrue(studentDao.Add(student, type).StudentId == student.StudentId);
+        }
+    }
+}
