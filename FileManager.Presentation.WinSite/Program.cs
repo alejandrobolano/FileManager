6677e54b81859668105df913b33d017f4b22dc8a
Alejandro Bolano <elbola.cuba1992@gmail.com>
2019-06-21 16:06:56 +0200

    [Issue6] commit by error

--- FileManager.Presentation.WinSite/Program.cs
+++ FileManager.Presentation.WinSite/Program.cs
@@ -16,7 +16,7 @@ namespace FileManager.Presentation.WinSite
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormAirports());
+            Application.Run(new FormStudent());
         }
     }
 }
